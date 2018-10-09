8058b02f27e329a55ae9371d79aa03bcb1392c7b
Mike Martin <mikemartin604@gmail.com>
2018-10-09 12:34:42 +1000

    remove viewport fix

--- app.js
+++ app.js
@@ -30,12 +30,3 @@ new Vue({
     }
   }
 })
-
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
